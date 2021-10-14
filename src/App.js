bb73e9b92f969971c35d4e79d3d42b9f81507247
Breno <bhfs.2908@gmail.com>
2021-10-14 10:46:27 -0300

    feat:add icons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import styles from "./App.module.scss";
 function App() {
   return (
     <div className={styles.container}>
-      <h1>Hello World</h1>
+      <nav className={styles.navbar}>
+        <div className="flex items-center">
+          <img className="rounded-full mx-3 my-1" width="35" height="35" src="/fb.svg" alt="facebook" />
+          <div className={styles.containerLogo}>
+            <img src="/search.svg" alt="busca" width="25" height="25" />
+            <input type="text" className={styles.searchFacebook} placeholder="Search Facebook" />
+          </div>
+        </div>
+        <div className="w-48 h-full flex justify-between">
+          <div className={`${styles.icons} border-b-2 border-blue-400`}>
+            <img width="25" height="25" src="/home.svg" alt="Tela inicial" />
+          </div>
+          <div className={styles.icons}>
+            <img width="25" height="25" src="/people.svg" alt="Tela inicial" />
+          </div>
+          <div className={styles.icons}>
+            <img width="25" height="25" src="/groups.svg" alt="Tela inicial" />
+          </div>
+        </div>
+        <div className="w-48 h-full"></div>
+      </nav>
     </div>
   );
 }
