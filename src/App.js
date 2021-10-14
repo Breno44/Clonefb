bf5c76cde0b3a0da635762fc7f0b2cf3d9163c63
Breno <bhfs.2908@gmail.com>
2021-10-14 09:06:29 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
