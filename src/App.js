8586a30bfce06b3506e5d07e8b4ba2f33d9e5308
Breno <bhfs.2908@gmail.com>
2021-10-14 16:04:00 -0300

    feat:left container

--- src/App.js
+++ src/App.js
@@ -38,6 +38,56 @@ function App() {
           </div>
         </div>
       </nav>
+      <div className="flex">
+        <div className="w-1/4 h-screen bg-gray-100">
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="rounded-full mx-3" src="https://i.pravatar.cc/40" alt="user" />
+            Breno Henrique Ferreira Silva
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/shield.svg" alt="" width="40" height="40" />
+            COVID-19 Information Center
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/people.svg" alt="" width="40" height="40" />
+            Find Friends
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/groups.svg" alt="" width="40" height="40" />
+            Groups
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/home1.svg" alt="" width="40" height="40" />
+            Marketplace
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/ondemand.svg" alt="" width="40" height="40" />
+            Videos
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/event.svg" alt="" width="40" height="40" />
+            Events
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/update.svg" alt="" width="40" height="40" />
+            Memories
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/turned.svg" alt="" width="40" height="40" />
+            Saved
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3" src="/flag.svg" alt="" width="40" height="40" />
+            Pages
+          </div>
+          <div className="flex text-md px-3 py-2 items-center hover:bg-gray-200 rounded-lg m-2">
+            <img className="mx-3 bg-gray-300 rounded-full px-1 py-1" src="/expand.svg" alt="" width="40" height="40" />
+            See more
+          </div>
+        </div>
+        <div className="flex-grow h-screen bg-green-300">this is a test</div>
+        <div className="w-1/4 h-screen bg-yellow-300">this is a test</div>
+      </div>
     </div>
   );
 }
